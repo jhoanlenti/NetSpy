4644a1fd06aad9ac5251ec5b013f202c54ef58c7
sergi-pons44 <sergipons444@gmail.com>
2025-01-09 11:00:19 +0100

    SCRUM-14 descripción

--- banner/ban.py
+++ banner/ban.py
@@ -16,3 +16,5 @@ def netspy_banner():
 """
     print(banner)
     print("\nBienvenido al Programa de NetSpy. Tu confianza es nuestra prioridad!!! :D")
+    print("\n Tool: Network Ports and IPs manangment")
+    print("\n\nAuthor: Sergi, Jordi, Ethan, Jhoan y Nuria")
